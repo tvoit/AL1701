2e4f3bd0ba8dfa47fa8f669ef634c925d80137d4
tvoit <128392639+tvoit@users.noreply.github.com>
2023-04-11 15:33:38 +0500

    Update flesh.sh

--- flesh.sh
+++ flesh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir /media/fleshka
-dd if=/dev/zero of=/tmp/fleshka bs=1M count=500 && mkfs.ext4 /tmp/fleshka && mount -o rw,user /tmp/fleshka /media/fleshka
+dd if=/dev/zero of=fleshka bs=1M count=500 && mkfs.ext4 fleshka && mount -o rw,user fleshka /media/fleshka
